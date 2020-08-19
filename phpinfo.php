691161ef4b2b5bbe944d5b96337113a77af568d3
aki016 <64180800+aki016@users.noreply.github.com>
2020-08-19 11:22:09 +0900

    Add files via upload

---
+++ phpinfo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="ja" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+		<link rel="stylesheet" href="">
+		<script type="text/javascript" src="js.js"></script>
+      <title></title>
+    </head>
+
+<body>
+<?php
+	phpinfo()
+?>
+</body>
+</html>
